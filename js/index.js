b24742b695e63c0f8efefe4a4969957f50e1a887
rjspyk5 <rjspyk5@gmail.com>
2024-02-19 11:38:55 +0600

    available seat,total seat funcionlaity added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// all needed variables
 let bookedSeat=[];
 const seatParent=document.getElementById("seat-parent");
 const totalPriceContainer=document.getElementById("totalPrice");
@@ -5,53 +6,80 @@ const ticketTable=document.getElementById("ticketTable");
 const copunField=document.getElementById("copun");
 const copunContainer=document.getElementById("copun-container");
 const copunApplyBtn=document.getElementById("copun-apply-btn");
+const bookedSeatCount=document.getElementById("bookedSeatCount");
 
 
-const bookSeat=(e)=>{
-    if ( e.target.className.includes("seat")) {
-        appendSeat(e,e.target.id)
-    }  
+
+
+
+
+// process to append tr and related functionality
+const appendSeat=(id)=>{ 
+    if ((!bookedSeat.includes(id)) && bookedSeat.length<4) {
+        bookedSeat.push(id);
+        document.getElementById(id).style.backgroundColor="#1DD100"
+        const tr=document.createElement("tr");
+        tr.classList.add("text-secondary");
+        tr.innerHTML=`
+        <td>${id}</td>
+        <td>Economy</td>
+        <td>550</td>
+        `
+        ticketTable.appendChild(tr) 
+    }
 }
 
+// setTotal price on totalPriceContainer
+const setTotalPrice=(price)=>{
+    totalPriceContainer.innerText=price;
+}
+// total price calculation
 const totalPrice=()=>{
     return bookedSeat.length*550
 }
-const appendSeat=(e,id)=>{
-    if ((!bookedSeat.includes(id)) && bookedSeat.length<4) {
-        bookedSeat.push(id);
-        e.target.style.backgroundColor="#1DD100"
-        const tr=document.createElement("tr");
-    tr.classList.add("text-secondary");
-    tr.innerHTML=`
-    <td>${id}</td>
-  <td>Economy</td>
-  <td>550</td>
-    `
-    ticketTable.appendChild(tr)
-    let total=totalPrice(id)
-    setTotalPrice(total)
-    
-        
-    }
-    
-
+// update Available seat
+function UpdateAvailableSeat() {
+    let available=40-bookedSeat.length;
+    document.getElementById("available-seat").innerText=available;
 }
 
-const copunCode=()=>{
-    console.log("object");
-    console.log(bookedSeat.length);
-    console.log(copunField.value);
+// updateBookedSeatCount
+function updateBookedSeatCount() {
+    bookedSeatCount.innerText=bookedSeat.length;
+}
+
+// //grand total
+// const grandTotal=(copun15,copun20)=>{
+//     let total=totalPrice();
+//       if (copun15) {
+//        return total*(85/100);
+        
+//       }
+//       if (copun20) {
+//         return total*(85/100);
+//       }
+//  return total;
+// }
+
+// What happened when someone clicked on apply copun
+const handleCopunCode=()=>{
     if (bookedSeat.length==4 && copunField.value==="NEW15") {
         copunContainer.classList.add("hidden")
     }
 }
-
-const setTotalPrice=(price)=>{
-    totalPriceContainer.innerText=price;
-
+// What happened when someone booked a seat
+const bookSeat=(e)=>{
+    if ( e.target.className.includes("seat")) {
+        appendSeat(e.target.id);
+        UpdateAvailableSeat();
+        updateBookedSeatCount();
+        setTotalPrice(totalPrice(e.target.id));   
+         // copun code functionality
+         if (bookedSeat.length===4) {
+            copunApplyBtn.removeAttribute('disabled');  
+        }
+    }  
 }
-
-
-
+// Event listner for seat booking and copun apply
+copunApplyBtn.addEventListener("click",handleCopunCode)
 seatParent.addEventListener("click",bookSeat)
-copunApplyBtn.addEventListener("click",copunCode)
